e5fcfe7683a1acf8a7b654fb866ad739e9764aca
Sudha <s.sudha19082006@gmail.com>
2025-08-26 19:05:25 +0530

    Create problem.java

---
+++ problem.java
@@ -0,0 +1,26 @@
+/*
+Okay this is a way to get input from the user 
+*/
+
+import java.util.*;
+
+class problem {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        // Take all inputs
+        // Print all the values
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        float b=sc.nextFloat();
+        long c=sc.nextLong();
+        byte d=sc.nextByte();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+        sc.close();
+    }
+}
