9548e9ec4457b6a3d07d9ff0f747770439f62a41
Sudha <s.sudha19082006@gmail.com>
2025-08-26 19:43:29 +0530

    Create Solution3.java

---
+++ Solution3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+    public void utility(Scanner scn) {
+      // In this method we dont import Scanner class 
+        // code here
+       // Scanner sc=new Scanner(System.in);
+        String a=scn.nextLine();
+        String b=scn.nextLine();
+        System.out.println(a+" "+b);
+    }
+}
