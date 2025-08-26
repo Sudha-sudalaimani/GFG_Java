220f4c2548bf46d18908100fecc57d24452cdb6c
Sudha <s.sudha19082006@gmail.com>
2025-08-26 12:35:17 +0530

    Create Geeks.java

---
+++ Geeks.java
@@ -0,0 +1,12 @@
+/*
+This is very simple program beacuse these also add in my gfg number of problems 😹
+*/
+class Geeks {
+
+    // Function to print hello
+    static void printHello() {
+
+        // Your code here
+        System.out.println("Hello World");
+    }
+}
