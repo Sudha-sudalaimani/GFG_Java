d2a17cf5faaf2e976e6be29549a382d0179e1059
Sudha <s.sudha19082006@gmail.com>
2025-08-27 18:17:08 +0530

    Create casting.java

---
+++ casting.java
@@ -0,0 +1,13 @@
+//This is typecasting in java simply (datatype)variable; that's all
+import java.io.*;
+import java.util.*;
+
+class casting {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        // Typecast and print
+        int result=(int)d;
+        System.out.println(result);
+    }
+}
