c9e30571925429f15a2ba8c6ae3245ad8474b806
Sudha <s.sudha19082006@gmail.com>
2025-08-27 17:45:50 +0530

    Create VowelorNot.java

---
+++ VowelorNot.java
@@ -0,0 +1,17 @@
+
+
+// User function Template for Java
+class VowelorNot {
+    String isVowel(char c) {
+        // code here
+        if(c=='a'|| c=='e'||c=='i'||c=='o'||c=='u'){
+             return "YES";
+        }
+        else if(c=='A'|| c=='E'||c=='I'||c=='O'||c=='U'){
+           return "YES";
+        }
+        else{
+             return "NO";
+        }
+    }
+}
