583c922aab7dc9e150a07fe0da8e8b2f8470fc3a
Sudha <s.sudha19082006@gmail.com>
2025-08-27 18:38:10 +0530

    Create IFstatement.java

---
+++ IFstatement.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class IFstatement {
+    public static void utility(int number) {
+        if(number<=100){
+            System.out.println("Number");
+        }
+        else{
+            System.out.println("Big");
+            System.out.println("Number");
+        }
+    }
+}
