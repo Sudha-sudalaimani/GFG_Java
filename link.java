25f27e72b2e24819b7fa46ec144e0ee15fbaa225
Sudha <s.sudha19082006@gmail.com>
2025-08-28 22:21:10 +0530

    Create link.java

---
+++ link.java
@@ -0,0 +1,14 @@
+//Concatenate Integers
+//Given two integers a and b, you need to concatenate them so the output is ab and print it.
+import java.lang.*;
+import java.util.*;
+
+class link {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Your Code to Concatenate and print
+        System.out.println(a+""+b);
+    }
+}
