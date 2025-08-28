8194bfaeedbd095f66c5dc6962eae5ae7696e923
Sudha <s.sudha19082006@gmail.com>
2025-08-28 22:40:44 +0530

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,14 @@
+//Even Odd Game
+
+// User function Template for Java
+class Game {
+    public static String utility(int n) {
+
+        // write your code here
+        if(n%2==0){
+            return "Friend";
+        }
+         return "You";
+    }
+   
+}
