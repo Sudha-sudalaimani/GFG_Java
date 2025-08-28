083777c279c47aaa33b93fdf02d9fab5d003157b
Sudha <s.sudha19082006@gmail.com>
2025-08-28 22:17:20 +0530

    Create INPUT.java

---
+++ INPUT.java
@@ -0,0 +1,15 @@
+//Input Integer
+//Given a number n. Take input and add 10 to its value and print it.
+
+import java.util.*;
+
+class INPUT {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n+10);
+    }
+}
