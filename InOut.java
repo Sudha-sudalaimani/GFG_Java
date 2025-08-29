4f5d7a98438c83131a1a6c883475135aa77ffd81
Sudha <s.sudha19082006@gmail.com>
2025-08-29 19:32:42 +0530

    Create InOut.java

---
+++ InOut.java
@@ -0,0 +1,10 @@
+//Java Input/Output
+//Just simple program
+// User function Template for Java
+class InOut {
+    static void printIntString(String S, int N) {
+        // code here
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+    }
+}
