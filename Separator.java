d47ab6ffcab4436ca288615b8d0a51dca71da064
Sudha <s.sudha19082006@gmail.com>
2025-08-29 19:46:17 +0530

    Create Separator.java

---
+++ Separator.java
@@ -0,0 +1,10 @@
+//Print With Separator
+public class Separator {
+    public static void utility(Scanner scn) {
+        String a = scn.nextLine();
+        String b = scn.nextLine();
+        String separator = scn.nextLine();
+        // print with seperator
+        System.out.println(a+separator+b);
+    }
+}
