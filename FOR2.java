fec692663e4a92b57a0a44d29e69f2c49a992414
Sudha <s.sudha19082006@gmail.com>
2025-08-30 19:32:39 +0530

    Create FOR2.java

---
+++ FOR2.java
@@ -0,0 +1,13 @@
+//For Loop - 1
+import java.util.Scanner;
+
+public class Solution {
+    public static void solve() {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i=i+1){
+            System.out.print(i*n+" ");
+        }
+    }
+}
