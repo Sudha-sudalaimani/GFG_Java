2214b0408d75c0d53dc851011a0c958a869a16df
Sudha <s.sudha19082006@gmail.com>
2025-08-31 19:46:01 +0530

    Create Without_Space.java

---
+++ Without_Space.java
@@ -0,0 +1,9 @@
+//Print Without Space
+class Without_Space {
+    public void utility(Scanner scn) {
+        // code here
+        String a=scn.nextLine();
+        String b=scn.nextLine();
+        System.out.println(a+b);
+    }
+}
