a01258d8d261e369611506f597e2de841ded9608
Sudha <s.sudha19082006@gmail.com>
2025-09-01 08:34:14 +0530

    Create While_Loop.java

---
+++ While_Loop.java
@@ -0,0 +1,10 @@
+//While Loop
+class While_Loop {
+    public static void utility(int x) {
+        // code here
+        while(x>=0){
+            System.out.print(x+" ");
+            x=x-1;
+        }
+    }
+}
