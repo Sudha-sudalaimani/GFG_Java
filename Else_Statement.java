7ed48ebfe1d8cad5d04ea3f9f0674466fa8bd63c
Sudha <s.sudha19082006@gmail.com>
2025-09-05 07:22:16 +0530

    Create Else_Statement.java

---
+++ Else_Statement.java
@@ -0,0 +1,16 @@
+//The Else Statement
+import java.util.*;
+
+public class Else_Statement {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<=100){
+            System.out.println("Number");
+        }
+        else{
+            System.out.println("Big");
+        }
+    }
+}
