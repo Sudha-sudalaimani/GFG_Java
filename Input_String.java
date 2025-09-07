e422c13e4f02b73d6a071ac140ba6c20e631ba50
Sudha <s.sudha19082006@gmail.com>
2025-09-07 22:21:34 +0530

    Create Input_String.java

---
+++ Input_String.java
@@ -0,0 +1,12 @@
+//Input String
+import java.io.*;
+import java.util.*;
+
+class Input_String {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+    }
+}
