2a74f6b73f656683a90434103694032a5d321480
Sudha <s.sudha19082006@gmail.com>
2025-09-07 22:29:00 +0530

    Create Type_Conversion.java

---
+++ Type_Conversion.java
@@ -0,0 +1,8 @@
+//Type Conversion double to int
+class Solution {
+    public static int typeCast(double d) {
+        // code here
+     //  double d=integer.valueOf(d);
+        return (int)d;
+    }
+}
