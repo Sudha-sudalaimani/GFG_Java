35bc42f8acd413578840a7d39d86ae38d55dd450
Sudha <s.sudha19082006@gmail.com>
2025-09-09 20:47:07 +0530

    Create Else_Statement1.java

---
+++ Else_Statement1.java
@@ -0,0 +1,15 @@
+//The Else Statement
+// User function Template for Java
+class Else_Statement1 {
+    public static void utility(int number) {
+        // Write your if-else statement below
+
+        // Write your if-else statement above
+        if(number>100){
+            System.out.println("Big");
+        }
+        else{
+            System.out.println("Number");
+        }
+    }
+}
