27d9f74627c0a5ec6f42739cfef7883faae9c30a
Sudha <s.sudha19082006@gmail.com>
2025-09-10 10:58:57 +0530

    Create Print_NewLine.java

---
+++ Print_NewLine.java
@@ -0,0 +1,9 @@
+//Print Without Newline
+class Print_NewLine  {
+    public void utility(Scanner sc) {
+        // code here
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        System.out.println(a+" "+b);
+    }
+}
