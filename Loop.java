8db3f010878b9886c0ff60070dc578c7f9397b30
Sudha <s.sudha19082006@gmail.com>
2025-09-10 11:07:03 +0530

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,14 @@
+//While Loop
+import java.util.*;
+
+public class Loop {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        // Your Code Here
+        int x=sc.nextInt();
+        while(x>=0){
+            System.out.print(x+" ");
+            x=x-1;
+        }
+    }
+}
