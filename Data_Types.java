ad0f8e5e751ac32a2b69bc6e84acf6394fcc09af
Sudha <s.sudha19082006@gmail.com>
2025-09-12 21:10:21 +0530

    Create Data_Types.java

---
+++ Data_Types.java
@@ -0,0 +1,25 @@
+//Java Basic Data Types
+
+import java.util.*;
+class Data_Types {
+
+    int javaIntType(Scanner sc) {
+        // code here
+        int a=sc.nextInt();
+        return a;
+    }
+
+    String javaStringType(Scanner sc) {
+        
+        // code here
+        String b=sc.next();
+        return b;
+    }
+
+        
+    float javaFloatType(Scanner sc) {
+        // code here
+        float c=sc.nextFloat();
+        return c;
+    }
+}
